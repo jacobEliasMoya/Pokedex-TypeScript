88b896cc77a6c3bb10a500fd0c1c4e3d6e67dfbc
jacob moya <jemoya1994@Gmail.com>
2024-09-18 22:03:35 -0700

    Worked on the entry to the pokedex, created entirely css/scss  pokeball w/ hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 function App() {
 
-  const [message, setMessage] = useState<boolean>(false);
+  // const [message, setMessage] = useState<boolean>(false);
 
-  const newMessage = () => message ? setMessage(false) : setMessage(true) ;
+  // const newMessage = () => message ? setMessage(false) : setMessage(true) ;
 
   return (
     <>
-      <h1 className='invert' >{message ? 'Clicked On' : 'Clicked Off'}</h1>
-      <button className="btn" onClick={newMessage}> {!message ? 'Not Clicked' : 'Clicked'} </button>
+
+    <div className="pokeball-container">
+      <div className="pokeball-inner">
+        <div className="pokeball-top">
+          <div className="pokeball-button">
+          </div>
+        </div>
+        <div className="pokeball-bottom"></div>
+      </div>
+    </div>
+
+      {/* <h1 className='invert' >{message ? 'NFL' : 'NFL+'}</h1>
+      <button className="btn" onClick={newMessage}> {!message ? 'Not Clicked' : 'Clicked'} </button> */}
     </>
   )
 }
