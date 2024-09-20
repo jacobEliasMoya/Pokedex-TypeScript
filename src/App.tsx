dcc30945a37a2634373057ef5f37810fc63b7c4f
jacob moya <jemoya1994@Gmail.com>
2024-09-19 20:48:43 -0700

    Worked in an api/chained to get random pokemon/ pokemon stats/ info/ imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-// import { useState } from 'react'
+import { useEffect,useState } from "react"
+
+
 
 function App() {
 
-  // const [message, setMessage] = useState<boolean>(false);
+  interface pokeArray {
+    name:string,
+    url:string
+  }
 
-  // const newMessage = () => message ? setMessage(false) : setMessage(true) ;
+  const [pokemonObj,setPokemon] = useState<pokeArray | any>();
 
+  const getPokemon = () => {
+    // worked into onclick to get random pokemon
+    fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2000')
+    .then(response=>response.json())
+    .then(res=> {
+      const pokeUrl = res.results[Math.floor(Math.random() * res.results.length)]; 
+      return fetch(pokeUrl.url)
+      })
+      .then(response => response.json())
+      .then(res=>{
+        setPokemon(res)
+        console.log(res)
+      })
+    }
+
+  
+  useEffect(()=>{
+    
+  },[pokemonObj])
+  
   return (
     <>
+      <h1> 
+        <img src={pokemonObj ? pokemonObj.sprites.front_default : '#'} alt="" /><br/>
+        <a href={pokemonObj ? pokemonObj.url : '#'} target="_blank" rel="noopener noreferrer"> {pokemonObj ? pokemonObj.name : 'Open Pokeball to Select Random Pokemon'}</a> 
+      </h1>
 
-    <div className="pokeball-container">
-      <div className="pokeball-inner">
-        <div className="pokeball-top">
-          <div className="pokeball-button">
+
+      <div className="pokeball-container">
+            <div className="pokeball-inner">
+              <div className="pokeball-top">
+                <div className="pokeball-button" onClick={getPokemon}>
+                </div>
+              </div>
+              <div className="pokeball-bottom"></div>
           </div>
-        </div>
-        <div className="pokeball-bottom"></div>
-      </div>
-    </div>
+      </div>     
 
-      {/* <h1 className='invert' >{message ? 'NFL' : 'NFL+'}</h1>
-      <button className="btn" onClick={newMessage}> {!message ? 'Not Clicked' : 'Clicked'} </button> */}
+
+
     </>
   )
 }
