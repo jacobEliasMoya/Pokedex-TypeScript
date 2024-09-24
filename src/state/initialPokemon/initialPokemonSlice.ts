8ec7cb896ad862fe096a1bb206e82a236682571c
jacob moya <jemoya1994@Gmail.com>
2024-09-23 23:57:42 -0700

    worked in types for intial pokemon array w/ respective typing, added store/ provider slices of state within redux

---
+++ src/state/initialPokemon/initialPokemonSlice.ts
@@ -0,0 +1,68 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface NameUrlType {
+    name:string,
+    url:string
+}
+
+interface AbilitiesArrObjType{
+    ability: NameUrlType,
+    is_hidden: boolean,
+    slot: number
+}
+
+interface CriesObjType{
+    latest: string,
+    legacy: string
+}
+
+interface GameIndecesType{
+    game_index:number,
+    version: NameUrlType
+
+}
+
+interface MovesType{
+    move: Array<NameUrlType>,
+}
+
+interface InitialPokeList {
+    abilities: Array<AbilitiesArrObjType>,
+    base_experience: number,
+    cries: CriesObjType,
+    forms: Array<NameUrlType>,
+    game_indeces: Array<GameIndecesType>,
+    height:number,
+    held_items: Array<any>,
+    id:number,
+    is_default:boolean,
+    location_area_encounters: string,
+    // fix tomorrow/ fill in full type
+    moves:Array<MovesType>,
+    name:string,
+    order:number,
+    // fix tomorrow/ fill in full type/ proper typing
+    past_abilities:Array<any>,
+    past_types:Array<any>,
+    species:NameUrlType,
+    sprites: any,
+    stats:Array<any>,
+    types:Array<any>,
+    url:string,
+    weight: number
+}
+
+const initialState:InitialPokeList|any = {}
+
+const intialPokeSlice = createSlice({
+    name: "pokemon",
+    initialState,
+    reducers:{
+        setIntialList : (state, action:PayloadAction<InitialPokeList>) =>{
+            state = {...state,...action.payload}
+        }
+    }
+
+})
+
+export default intialPokeSlice.reducer;
